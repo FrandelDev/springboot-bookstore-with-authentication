a37c533c4d0b643cfc51df13162ff91352436fcc
Frandel <frandel-c.r@outlook.com>
2024-05-11 17:49:24 -0400

    reordering files

---
+++ src/bookstore_recommendation_of_books/src/main/java/com/bookstore_recommendation_of_books/model/Book.java
@@ -0,0 +1,116 @@
+package com.bookstore_recommendation_of_books.model;
+
+import java.util.List;
+import java.util.Optional;
+
+public class Book {
+
+    String id;
+    String title;
+    Optional<String> subtitle;
+    String description;
+    String authors;
+    String publisher;
+    Integer pages;
+    Integer year;
+    String image;
+    Double price;
+    Double priceWithDiscount;
+    List<String> categories;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Optional<String> getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(Optional<String> subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String authors) {
+        this.authors = authors;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+
+    public void setPages(Integer pages) {
+        this.pages = pages;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getPriceWithDiscount() {
+        return priceWithDiscount;
+    }
+
+    public void setPriceWithDiscount(Double priceWithDiscount) {
+        this.priceWithDiscount = priceWithDiscount;
+    }
+
+    public List<String> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<String> categories) {
+        this.categories = categories;
+    }
+}
