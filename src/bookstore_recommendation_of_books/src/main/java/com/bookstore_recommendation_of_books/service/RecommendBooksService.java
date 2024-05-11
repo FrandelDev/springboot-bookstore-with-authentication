1d676d57d07e1697b55d444723e8cf493cb20ac0
Frandel <frandel-c.r@outlook.com>
2024-05-11 18:04:56 -0400

    getBooksCategories

--- src/bookstore_recommendation_of_books/src/main/java/com/bookstore_recommendation_of_books/service/RecommendBooksService.java
+++ src/bookstore_recommendation_of_books/src/main/java/com/bookstore_recommendation_of_books/service/RecommendBooksService.java
@@ -4,21 +4,21 @@ import com.bookstore_recommendation_of_books.model.Book;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 
 @Service
 public class RecommendBooksService {
-    public static void getBooksCategories(){
+    public static List<String> getBooksCategories(){
         RestTemplate template = new RestTemplate();
         List<String> allCategories = new ArrayList<>();
-        List<Map<String, Book>> result = template.getForObject("http://localhost:8383/api/bookstore", List.class);
+        List<Map<String, Object>> result = template.getForObject("http://localhost:8383/api/bookstore", List.class);
 
+        result.forEach(book ->{
+          allCategories.addAll((List<String>)book.get("categories"));
+        });
 
-        System.out.println(result.toArray().length);
+        return allCategories;
 
     }
 }
