f9609bea6619c6d19615ac3af4e723edcc0c57e1
Frandel <frandel-c.r@outlook.com>
2024-05-11 20:33:52 -0400

    get the books id to recommend

--- src/bookstore_recommendation_of_books/src/main/java/com/bookstore_recommendation_of_books/BookstoreRecommendationOfBooksApplication.java
+++ src/bookstore_recommendation_of_books/src/main/java/com/bookstore_recommendation_of_books/BookstoreRecommendationOfBooksApplication.java
@@ -4,14 +4,16 @@ import com.bookstore_recommendation_of_books.service.RecommendBooksService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class BookstoreRecommendationOfBooksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreRecommendationOfBooksApplication.class, args);
-		System.out.println("hola");
 
-		RecommendBooksService.getBooksCategories();
+
 	}
+
 
 }
--- src/bookstore_recommendation_of_books/src/main/java/com/bookstore_recommendation_of_books/service/RecommendBooksService.java
+++ src/bookstore_recommendation_of_books/src/main/java/com/bookstore_recommendation_of_books/service/RecommendBooksService.java
@@ -1,6 +1,6 @@
 package com.bookstore_recommendation_of_books.service;
 
-import com.bookstore_recommendation_of_books.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,8 +9,46 @@ import java.util.*;
 
 @Service
 public class RecommendBooksService {
+
+    private static RestTemplate template = new RestTemplate();
+
+
+
+    public static void recommendedBooksAndApplyDiscounts(){
+
+
+    }
+
+    public static List<String> getBooksIdToRecommend(){
+        var categories = Arrays.asList(peekCategories());
+        List<String> booksId = new ArrayList<>();
+
+        categories.forEach(category ->{
+            Map<String,Object> res = template.getForObject("https://www.dbooks.org/api/search/"+category.toLowerCase().replaceAll(" ","+"),Map.class);
+
+            String total =  res.get("total").toString();
+            if(res.get("status") != "not found"){
+            List<Map<String,Object>> books = (List<Map<String,Object>>) res.get("books");
+            booksId.add((String) books.get(new Random().nextInt(0, Integer.valueOf(total))).get("id"));
+            }
+        });
+
+        return booksId;
+    }
+
+    public static String[]  peekCategories(){
+        var categories = getBooksCategories();
+        String[] categoriesForSearchCriteria = new String[7];
+
+        for (int i = 6; i >= 0; i--){
+            int categoryIndex = new Random().nextInt(0,categories.toArray().length);
+            categoriesForSearchCriteria[i] = categories.get(categoryIndex);
+        }
+        return categoriesForSearchCriteria;
+    }
+
     public static List<String> getBooksCategories(){
-        RestTemplate template = new RestTemplate();
+
         List<String> allCategories = new ArrayList<>();
         List<Map<String, Object>> result = template.getForObject("http://localhost:8383/api/bookstore", List.class);
 
